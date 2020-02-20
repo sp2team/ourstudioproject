cf41cdac58ba09b7a4ecf68d2b0b5fa49a74543a
NinjaWotterz <54068150+NinjaWotterz@users.noreply.github.com>
2020-02-20 17:33:08 +0800

    Revert "update"
    This reverts commit a02e6692632d4dd32644a7fb1985397033950dae.

--- DM2122_Week16_Solution/DM2122 Prac/Source/Camera2.cpp
+++ DM2122_Week16_Solution/DM2122 Prac/Source/Camera2.cpp
@@ -5,6 +5,7 @@
 
 Camera2::Camera2()
 {
+
 }
 
 Camera2::~Camera2()
@@ -15,19 +16,17 @@ void Camera2::Init(const Vector3& pos, const Vector3& target, const Vector3& up)
 {
 	this->position = defaultPosition = pos;
 	this->target = defaultTarget = target;
-	view = (target - position).Normalized();
-	right = view.Cross(up);
+	Vector3 view = (target - position).Normalized();
+	Vector3 right = view.Cross(up);
 	right.y = 0;
 	right.Normalize();
 	this->up = defaultUp = right.Cross(view).Normalized();
-	height = pos.y;
 }
 
 void Camera2::Update(double dt)
 {
-	/*
 	static const float CAMERA_SPEED = 50.f;
-	if(Application::IsKeyPressed(VK_LEFT) || Application::IsKeyPressed('A'))
+	if(Application::IsKeyPressed(VK_LEFT))
 	{
 		float yaw = (float)(-CAMERA_SPEED * dt);
 		Mtx44 rotation;
@@ -35,7 +34,7 @@ void Camera2::Update(double dt)
 		position = rotation * position;
 		up = rotation * up;
 	}
-	if(Application::IsKeyPressed(VK_RIGHT) || Application::IsKeyPressed('D'))
+	if(Application::IsKeyPressed(VK_RIGHT))
 	{
 		float yaw = (float)(CAMERA_SPEED * dt);
 		Mtx44 rotation;
@@ -43,7 +42,7 @@ void Camera2::Update(double dt)
 		position = rotation * position;
 		up = rotation * up;
 	}
-	if(Application::IsKeyPressed(VK_UP) || Application::IsKeyPressed('W'))
+	if(Application::IsKeyPressed(VK_UP))
 	{
 		float pitch = (float)(-CAMERA_SPEED * dt);
 		Vector3 view = (target - position).Normalized();
@@ -55,7 +54,7 @@ void Camera2::Update(double dt)
 		rotation.SetToRotation(pitch, right.x, right.y, right.z);
 		position = rotation * position;
 	}
-	if(Application::IsKeyPressed(VK_DOWN) || Application::IsKeyPressed('S'))
+	if(Application::IsKeyPressed(VK_DOWN))
 	{
 		float pitch = (float)(CAMERA_SPEED * dt);
 		Vector3 view = (target - position).Normalized();
@@ -67,174 +66,6 @@ void Camera2::Update(double dt)
 		rotation.SetToRotation(pitch, right.x, right.y, right.z);
 		position = rotation * position;
 	}
-	*/
-	
-	static const float CAMERA_SPEED = 40.f;
-	view = (target - position).Normalized();
-	right = view.Cross(up);
-	Vector3 up2nd = Vector3(0, 1, 0);
-	right.y = 0;
-	float initalposy = position.y;
-
-	if (Application::IsKeyPressed(VK_LEFT))
-	{
-		up = right.Cross(view).Normalized();
-		float yaw = (float)(CAMERA_SPEED * dt * 2);
-		Mtx44 rotation;
-		rotation.SetToRotation(yaw, up2nd.x, up2nd.y, up2nd.z);
-		view = rotation * view;
-		target = position + view;
-		Rotation2 += yaw;
-	}
-	if (Application::IsKeyPressed(VK_RIGHT))
-	{
-		up = right.Cross(view).Normalized();
-		float yaw = (float)(-CAMERA_SPEED * dt * 2);
-		Mtx44 rotation;
-		rotation.SetToRotation(yaw, up2nd.x, up2nd.y, up2nd.z);
-		view = rotation * view;
-		target = position + view;
-		Rotation2 += yaw;
-	}
-	if (Application::IsKeyPressed(VK_UP))
-	{
-		float pitch = (float)(CAMERA_SPEED * dt);
-		Mtx44 rotation;
-		rotation.SetToRotation(pitch, right.x, 0, right.z);
-		view = rotation * view;
-		up = rotation * up;
-		target = position + view;
-		Rotation += pitch;
-
-	}
-	if (Application::IsKeyPressed(VK_DOWN))
-	{
-		float pitch = (float)(-CAMERA_SPEED * dt);
-		Mtx44 rotation;
-		rotation.SetToRotation(pitch, right.x, 0, right.z);
-		view = rotation * view;
-		up = rotation * up;
-		target = position + view;
-		Rotation += pitch;
-	}
-
-	if (Application::IsKeyPressed('A'))
-	{
-		Vector3 newposition = position - right;
-		bool nocollisionx = true;
-		bool nocollisionz = true;
-		bool nocollision = true;
-		if (newposition.z <= -200 || newposition.z >= 200)
-		{
-			nocollisionz = false;
-		}
-		if (newposition.x <= -200 || newposition.x >= 200)
-		{
-			nocollisionx = false;
-		}
-
-		if (nocollisionx == true && nocollision == true)
-		{
-			position.x = newposition.x;
-			target.x = position.x + view.x;
-		}
-		if (nocollisionz == true && nocollision == true)
-		{
-			position.z = newposition.z;
-			target.x = position.x + view.x;
-
-		}
-		target = position + view;
-
-	}
-	if (Application::IsKeyPressed('D'))
-	{
-		Vector3 newposition = position + right;
-		bool nocollisionx = true;
-		bool nocollisionz = true;
-		bool nocollision = true;
-		if (newposition.z <= -200 || newposition.z >= 200)
-		{
-			nocollisionz = false;
-		}
-		if (newposition.x <= -200 || newposition.x >= 200)
-		{
-			nocollisionx = false;
-		}
-
-		if (nocollisionx == true && nocollision == true)
-		{
-			position.x = newposition.x;
-			target.x = position.x + view.x;
-		}
-		if (nocollisionz == true && nocollision == true)
-		{
-			position.z = newposition.z;
-			target.z = position.z + view.z;
-		}
-
-	}
-
-	if (Application::IsKeyPressed('W'))
-	{
-		Vector3 newposition = position + view;
-		bool nocollisionx = true;
-		bool nocollisionz = true;
-		bool nocollision = true;
-		if (newposition.z <= -200 || newposition.z >= 200)
-		{
-			nocollisionz = false;
-		}
-		if (newposition.x <= -200 || newposition.x >= 200)
-		{
-			nocollisionx = false;
-		}
-
-		if (nocollisionx == true && nocollision == true)
-		{
-			position.x = newposition.x;
-			target.x = position.x + view.x;
-		}
-		if (nocollisionz == true && nocollision == true)
-		{
-			position.z = newposition.z;
-			target.z = position.z + view.z;
-
-		}
-
-		position.y = initalposy;
-	}
-	if (Application::IsKeyPressed('S'))
-	{
-		Vector3 newposition = position - view;
-		bool nocollisionx = true;
-		bool nocollisionz = true;
-		bool nocollision = true;
-
-		if (newposition.z <= -200 || newposition.z >= 200)
-		{
-			nocollisionz = false;
-		}
-		if (newposition.x <= -200 || newposition.x >= 200)
-		{
-			nocollisionx = false;
-		}
-
-		if (nocollisionx == true && nocollision == true)
-		{
-			position.x = newposition.x;
-			target.x = position.x + view.x;
-		}
-		if (nocollisionz == true && nocollision == true)
-		{
-			position.z = newposition.z;
-			target.z = position.z + view.z;
-
-		}
-		position.y = initalposy;
-	}
-
-	/*
 	if(Application::IsKeyPressed('N'))
 	{
 		Vector3 direction = target - position;
@@ -249,9 +80,7 @@ void Camera2::Update(double dt)
 		Vector3 view = (target - position).Normalized();
 		position -= view * (float)(10.f * dt);
 	}
-	*/
-
-	if (Application::IsKeyPressed('R'))
+	if(Application::IsKeyPressed('R'))
 	{
 		Reset();
 	}
@@ -262,4 +91,51 @@ void Camera2::Reset()
 	position = defaultPosition;
 	target = defaultTarget;
 	up = defaultUp;
+}
+
+void Camera2::MouseControl()
+{
+	Vector3 view = (target - position).Normalized();
+	Vector3 right = view.Cross(up).Normalized();
+	POINT mouseposition;
+	GetCursorPos(&mouseposition);
+
+	int positionX = mouseposition.x;
+	int positionY = mouseposition.y;
+
+	if (MouseState)
+	{
+		lastX = positionX;
+		lastY = positionY;
+		MouseState = false;
+	}
+
+	float offsetx = positionX - lastX;
+	float offsety = positionY - lastY;
+	lastX = positionX;
+	lastY = positionY;
+
+	float sens = 0.3f; // sensitivity of mouse
+	offsetx *= sens;
+	offsety *= sens;
+
+	yaw += offsetx; // for left and right 
+	pitch -= offsety; // for up and down
+
+	if (pitch > 89.0f) // to set the limit to how far the player can look around
+		pitch = 89.0f;
+	if (pitch < -89.0f)
+		pitch = -89.0f;
+
+	Vector3 front;
+	front.x = cos(Math::DegreeToRadian(yaw)) * cos(Math::DegreeToRadian(pitch));
+	front.y = sin(Math::DegreeToRadian(pitch));
+	front.z = sin(Math::DegreeToRadian(yaw)) * cos(Math::DegreeToRadian(pitch));
+
+	view = front.Normalized();
+	right.y = 0;
+	right.Normalize();
+	up = right.Cross(view).Normalized();
+	right = view.Cross(up).Normalized();
+	target = position + view;
 }
--- DM2122_Week16_Solution/DM2122 Prac/Source/Camera2.h
+++ DM2122_Week16_Solution/DM2122 Prac/Source/Camera2.h
@@ -6,24 +6,27 @@
 class Camera2 : public Camera
 {
 public:
+	//Vector3 position;
+	//Vector3 target;
+	//Vector3 up;
+
 	Vector3 defaultPosition;
 	Vector3 defaultTarget;
 	Vector3 defaultUp;
 
-	float Rotation;
-	float Rotation2;
-	float height;
-	bool mouseenabled;
-	float sensitivity;
-
-	Vector3 view;
-	Vector3 right;
+	bool MouseState = true;
+	float lastX = 400.f;
+	float lastY = 300.f;
+	float yaw = -90.0f;
+	float pitch = 0.0f;
+	float fov = 45.0f;
 
 	Camera2();
 	~Camera2();
 	virtual void Init(const Vector3& pos, const Vector3& target, const Vector3& up);
 	virtual void Update(double dt);
 	virtual void Reset();
+	void MouseControl();
 };
 
 #endif
