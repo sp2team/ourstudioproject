90e47f3ad1629f9d48c477daaece29c1e7a388e0
Saviorloh <Wesly_loh95@yahoo.com.sg>
2020-02-25 13:02:36 +0800

    The original master file
    original one

--- DM2122_Week16_Solution/DM2122 Prac/Source/SceneManager.cpp
+++ DM2122_Week16_Solution/DM2122 Prac/Source/SceneManager.cpp
@@ -14,8 +14,11 @@ SceneManager::~SceneManager()
 void SceneManager::Init()
 {
 	switchScene = 0;
-	scene = new SceneText;
-	scene->Init();
+	scene1 = new SceneText;
+	scene2 = new SceneText2;
+	scene = scene1;
+	scene1->Init();
+	scene2->Init();
 }
 
 void SceneManager::RenderScreen(double time)
@@ -23,33 +26,38 @@ void SceneManager::RenderScreen(double time)
 	//Clear color & depth buffer every frame
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	if (switchScene == 0)
-	{
-		//Render Single Screen
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glViewport(0, 0, 1920, 1000);
-		scene->Render();
-		scene->Update(time);
-	}
-	else if (switchScene == 1)
-	{
+	//if (switchScene == 0)
+	//{
+	//	//Render Single Screen
+	//	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	//	glViewport(0, 0, 1920, 1000);
+	//	scene->Render();
+	//	scene->Update(time);
+	//}
+	//else if (switchScene == 1)
+	//{
 		//Render Split Screen
 		glViewport(1920 / 2, 0, 1920 / 2, 1000);
-		scene->RenderRightScreen();
+		scene1->RenderRightScreen();
 		scene->Update(time);
 
 		glViewport(0, 0, 1920 / 2, 1000);
-		scene->RenderLeftScreen();
+		scene2->RenderLeftScreen();
 		scene->Update(time);
-	}
+	//}
 }
 
 void SceneManager::SwitchScreen()
 {
 	switchScene = scene->SwitchScene();
+	if (switchScene == 0)
+		scene = scene1;
+	else if (switchScene == 1)
+		scene = scene2;
 }
 
 void SceneManager::Exit()
 {
-	scene->Exit();
+	scene1->Exit();
+	scene2->Exit();
 }
--- DM2122_Week16_Solution/DM2122 Prac/Source/SceneManager.h
+++ DM2122_Week16_Solution/DM2122 Prac/Source/SceneManager.h
@@ -1,10 +1,13 @@
 #pragma once
 #include "SceneText.h"
+#include "SceneText2.h"
 
 class SceneManager
 {
 private:
 	Scene* scene;
+	Scene* scene1;
+	Scene* scene2;
 	int switchScene;
 
 public:
